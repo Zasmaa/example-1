d48b283f8d3db87cb68613b27726ad49dace47a4
Zasmaa <39232461+Zasmaa@users.noreply.github.com>
2018-11-12 01:15:31 -0600

    Add files via upload

---
+++ Components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+class Header extends React.Component{
+
+
+	 render() {
+    return (
+    	<h1>Neighbooh App</h1>
+    	)
+  }
+
+}
+
+export default Header;
