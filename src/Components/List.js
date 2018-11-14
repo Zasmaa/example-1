976713835a2ee2d71eb2f75e76808af0210688e8
Zasmaa <39232461+Zasmaa@users.noreply.github.com>
2018-11-14 04:22:00 -0600

    Update List.js

--- src/Components/List.js
+++ src/Components/List.js
@@ -3,6 +3,9 @@ import { slide as Menu } from 'react-burger-menu'
 
 class List extends React.Component {
 
+  
+ 
+
  
 
  
@@ -12,14 +15,18 @@ class List extends React.Component {
           <Menu>
             <div id="List">
                 <h2>Locations</h2>
+                <p> {this.props.queryString} </p>
                 <input type="text" value={this.props.queryString} onChange={e => this.props.handleChange(e.target.value)} />
                 <ol>
                     {this.props.locations.map(loc => {
                         return (
-                            <li key={loc.venue.id}>
+                            <li key={loc.venue.id}
+                           >
                                 <div>
                                     <p className="title"> 
-                                    Name: <a href="#">{loc.venue.name}</a>
+                                    Name:{""}
+                                     <a href="#" onClick={() => this.props. showInfoContent(loc)}>{loc.venue.name}</a>
+                                    
                                     </p>
                                     <p> Address: {loc.venue.location.address }</p>
 
